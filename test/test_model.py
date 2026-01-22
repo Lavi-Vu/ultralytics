4a2a29b207c7598321370fc72b8b85ff9334d687
Lavi-Vu <vunguyen2472001@gmail.com>
2026-01-22 10:18:41 +0700

    modify small models

--- test/test_model.py
+++ test/test_model.py
@@ -1,11 +1,17 @@
 import cv2
 from ultralytics import YOLO
-
+from copy import deepcopy
+from ultralytics.nn.tasks import parse_model
 # 🔧 Load your models
-# model = YOLO("/media/lavi/Data/ultralytics/ultralytics/cfg/models/MobileNetV3/MobileNetV3.yaml")
+model = YOLO("/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/pvn_detect.yaml")
+# model= YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/11/yolo11n.yaml')
 # model = YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolov8-convnext.yaml')
-# model = YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolo_MobileVitxxs.yaml')
-model= YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolov8-fasternet.yaml')
+# model = YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolov8-MobileNeXt.yaml')
+# model= YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolov8-fasternet.yaml')
+# model= YOLO('/media/lavi/Data/ultralytics/ultralytics/cfg/models/v8/yolov8-EfficentNet.yaml')
 
-model.export(format="onnx", opset=11, imgsz=512) # Export the model to ONNX format
-model.info()  # Print model information
+# model.info(verbose=True, detailed=False)  # Print model information
+_, _ = parse_model(deepcopy(model.yaml), ch=3, verbose=True)
+model.info()
+model.export(format="onnx", opset=11) # Export the model to ONNX format
+# print(model)
